2583bf9939b07abfc84ddd34d31c3de7b39e9cd3
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-09-23 12:54:17 +0600

    add two mongoose middleware into user schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -5,4 +6,26 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
+// Mongoose middleware
+userSchema.pre("save", async function (next) {
+  // this-> every single instance of userSchema
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (error) {
+    next(error);
+  }
+});
+userSchema.methods.comparePassword = async function (password, next) {
+  try {
+    const isMatched = await bcrypt.compare(password, this.password);
+    return isMatched;
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = mongoose.model("User", userSchema);
